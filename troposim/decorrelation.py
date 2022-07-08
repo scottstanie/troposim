66f596d4df75489b12e374a5419b33ebdddbc9d6
Scott Staniewicz <scott.j.staniewicz@jpl.nasa.gov>
2022-07-07 22:21:09 -0500

    add docs and rounding thresh

--- troposim/decorrelation.py
+++ troposim/decorrelation.py
@@ -1,28 +1,83 @@
 """Simulate phase noise from decorrelation."""
-from collections import Counter
-
 import numpy as np
+from tqdm import tqdm
 
 try:
     from scipy import special as sc
     from scipy import stats
 except ImportError:
     print("scipy not installed; needed for simulating decorrelation")
+    raise
 
 
-def phase_pdf(coherence, looks, phi0=0.0, nbins=200):
+def simulate(coherence, looks, nbins=200, rounding_threshold=0.05):
+    """Simulate decorrelation noise from a given coherence.
+
+    Parameters
+    ----------
+    coherence : int, ndarray
+        Coherence magnitude of phase noise.
+    looks : int
+        Number of looks.
+    nbins : int, optional
+        Granularity of phi bins, used for PDF generation. Defaults to 200.
+    rounding_threshold : float, optional
+        Threshold for rounding coherence to nearest multiple. Defaults to 0.05.
+        Rounding occurs to speed up PDF calculations, since nearby coherence values
+        produce nearly identical PDFs.
+
+    Returns
+    -------
+    ndarray: Decorrelation phase noise. Shape is same as `coherence`
+
+    """
+    out = np.zeros(coherence.shape)
+    coh_rounded = _round_to(np.atleast_1d(coherence), rounding_threshold)
+    coh_rounded = np.clip(coh_rounded, 0.01, 0.99)
+    looks = np.clip(looks, 1, 100)
+
+    phi_bins = np.linspace(-np.pi, np.pi, nbins + 1, endpoint=True)
+    # Will have a max of 100 unique coherence values
+    unique_cohs, counts = np.unique(coh_rounded, return_counts=True)
+    _, pdfs = phase_pdf(unique_cohs, looks, nbins=nbins, phi0=0.0)
+    for pdf, coh, count in tqdm(zip(pdfs, unique_cohs, counts), total=len(unique_cohs)):
+        samps = _sample_noise(phi_bins, pdf, size=count)
+        idxs = coh_rounded == coh
+        out[idxs] = samps
+    return out
+
+
+def phase_pdf(coherence, looks, nbins=200, phi0=0.0):
     """Compute the PDF of decorrleation phase noise for a given number of looks.
 
-    Uses Eq. 4.2.24 from Hanssen, 2001
+    Uses Eq. 4.2.24 from Hanssen, 2001, using the expression derived in
+    Barber (1993), Lee et al. (1994), and Joughin and Winebrenner (1994)
+
+    Parameters
+    ----------
+    coherence : int, ndarray
+        Coherence magnitude of phase noise.
+    looks : int
+        Number of looks.
+    nbins : int, optional
+        Granularity of phi bins, used for PDF generation. Defaults to 200.
+    phi0 : float, optional
+        Center phase. Defaults to 0.0.
+
+    Returns
+    -------
+    phi : ndarray
+        Phase bins at which the PDF is evaluated.
+    pdf : ndarray
+        Decorrelation phase noise. Shape is same as `coherence`
     """
     from numpy import cos, pi
 
     coherence = np.atleast_1d(coherence)
-    # phi = np.linspace(0, 2 * np.pi, nbins)
     phi = np.linspace(-pi, pi, nbins)
-    print(coherence.shape, phi.shape)
     # Add extra axis at end to make broadcasting work
     coherence = coherence[..., np.newaxis]
+    # Keep adding extra axes to phi to match coherence.shape
     for ndim in [1, 2, 3]:
         if coherence.ndim > ndim:
             phi = phi[np.newaxis, :]
@@ -37,30 +92,13 @@ def phase_pdf(coherence, looks, phi0=0.0, nbins=200):
 
 
 def _sample_noise(phi_bins, pdf, size=1):
-    """Sample decorrelation phase noise for a given number of looks.
+    """Sample decorrelation phase noise for a given pdf, using `phase_pdf`.
 
-    Uses Eq. 4.2.24 from Hanssen, 2001
-    """
+    Length of phi_bins must be 1 greater than `len(pdf)`."""
     # phi, pdf = phase_pdf(coherence, looks, phi0, nbins)
     dist = stats.rv_histogram((pdf.ravel(), phi_bins))
     return dist.rvs(size=size)
 
 
-def simulate(coherence, looks, phi0=0.0, nbins=200):
-    out = np.zeros(coherence.shape)
-    coh_rounded = np.clip(np.atleast_1d(coherence).round(2), 0.01, 0.99)
-    looks = np.clip(looks, 1, 100)
-
-    phi_bins = np.linspace(-np.pi, np.pi, nbins + 1, endpoint=True)
-    # Will have a max of 100 unique coherence values
-    unique_cohs, counts = np.unique(coh_rounded, return_counts=True)
-    # coh_counter = Counter(coh_rounded.ravel())
-    _, pdfs = phase_pdf(unique_cohs, looks, phi0, nbins)
-    for pdf, coh, count in zip(pdfs, unique_cohs, counts):
-        samps = _sample_noise(phi_bins, pdf, size=count)
-        idxs = coh_rounded == coh
-        out[idxs] = samps
-    return out
-
-
-# psd1d = ndimage.mean(psd2d, r, index=np.arange(0, num_r))
+def _round_to(x, step):
+    return step * np.round(x / step)
