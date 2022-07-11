8b2498e2b0997a82005af35360becd501cb34a2c
Scott Staniewicz <scott.j.staniewicz@jpl.nasa.gov>
2022-07-11 17:17:12 -0500

    comments for decor

--- src/troposim/decorrelation.py
+++ src/troposim/decorrelation.py
@@ -34,6 +34,7 @@ def simulate(coherence=None, looks=1, nbins=200, rounding_threshold=0.05):
     (Derived by Barber, 1993, Lee et al., 1994, Joughin and Winebrenner, 1994)
     """
     out = np.zeros(coherence.shape)
+    # Get a truncated list, for a max of (1 / rounding_threshold) unique values
     coh_rounded = _round_to(np.atleast_1d(coherence), rounding_threshold)
     if np.any(coh_rounded < 0) or np.any(coh_rounded > 1):
         raise ValueError("Coherence must be between 0 and 1")
@@ -42,11 +43,14 @@ def simulate(coherence=None, looks=1, nbins=200, rounding_threshold=0.05):
     looks = np.clip(looks, 1, 100)
 
     phi_bins = np.linspace(-np.pi, np.pi, nbins + 1, endpoint=True)
-    # Will have a max of 100 unique coherence values
     unique_cohs, counts = np.unique(coh_rounded, return_counts=True)
+    # Get the PDF for each unique coherence
     _, pdfs = phase_pdf(unique_cohs, looks, nbins=nbins, phi0=0.0)
     for pdf, coh, count in tqdm(zip(pdfs, unique_cohs, counts), total=len(unique_cohs)):
+        # Simulate a number of noise samples equal to the 
+        # number of pixels with this coherence
         samps = _sample_noise(phi_bins, pdf, size=count)
+        # Insert these at the appropriate locations in the output array
         idxs = coh_rounded == coh
         out[idxs] = samps
     return out
@@ -54,7 +58,7 @@ def simulate(coherence=None, looks=1, nbins=200, rounding_threshold=0.05):
 
 def phase_pdf(coherence, looks, nbins=200, phi0=0.0):
     """Compute the PDF of decorrleation phase noise for a given number of looks.
-    
+
     Uses Eq. 4.2.24 from Hanssen, 2001, using the expression derived in
     Barber (1993), Lee et al. (1994), and Joughin and Winebrenner (1994)
 
@@ -72,7 +76,7 @@ def phase_pdf(coherence, looks, nbins=200, phi0=0.0):
     Returns
     -------
 
-    
+
     """
     from numpy import cos, pi
     from scipy import special as sc
@@ -97,15 +101,15 @@ def phase_pdf(coherence, looks, nbins=200, phi0=0.0):
 
 def _sample_noise(phi_bins, pdf, size=1):
     """Sample decorrelation phase noise for a given pdf, using `phase_pdf`.
-    
+
     Length of phi_bins must be 1 greater than `len(pdf)`.
 
     Parameters
     ----------
     phi_bins :
-        
+
     pdf : ndarray
-        
+
     size : int or tuple of ints, optional
          (Default value = 1)
 
@@ -126,9 +130,9 @@ def _round_to(x, step):
     Parameters
     ----------
     x :
-        
+
     step :
-        
+
 
     Returns
     -------
