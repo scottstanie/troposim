c98f97f325559bbdf0879563aa0429415eb92a4a
Scott Staniewicz <scott.stanie@gmail.com>
2025-08-11 15:39:05 -0400

    Fix userwarning for import error

--- src/troposim/__init__.py
+++ src/troposim/__init__.py
@@ -1,14 +1,13 @@
 # -*- coding: utf-8 -*-
-from pkg_resources import DistributionNotFound, get_distribution
+try:
+    from importlib.metadata import version, PackageNotFoundError
+except ImportError:
+    from importlib_metadata import version, PackageNotFoundError
 
 try:
-    # Change here if project is renamed and does not equal the package name
-    dist_name = __name__
-    __version__ = get_distribution(dist_name).version
-except DistributionNotFound:
+    __version__ = version("troposim")
+except PackageNotFoundError:
     __version__ = "unknown"
-finally:
-    del get_distribution, DistributionNotFound
 
 # Most used modules should get auto-import
 from . import turbulence
